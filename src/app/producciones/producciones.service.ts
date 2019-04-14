57174f5db80c1923b5d296354b18673b68e13418
JuanFernandoCastaneda <jf.castaneda@uniandes.edu.co>
2019-04-14 09:39:36 -0500

    Corrección de errores 2

--- src/app/producciones/producciones.service.ts
+++ src/app/producciones/producciones.service.ts
@@ -1,6 +1,8 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
+import {Multimedia} from '../multimedia/multimedia';
+import {Productora} from '../productoras/productora';
 import {Produccion} from './produccion';
 
 const API_URL = 'http://localhost:8080/s1_foros-api/api/producciones/';
@@ -30,15 +32,17 @@ export class ProduccionesService {
     /**
      * Registra un nuevo producción
      * @param produccion entidad
+     * @param multimedia
+     * @param productora
      */
-    registrarProduccion(produccion: Produccion): Promise<any> {
+    registrarProduccion(produccion: Produccion, multimedia: Multimedia, productora: Productora): Promise<any> {
         const cuerpo = {
             nombre: produccion.nombre,
             descripcion: produccion.descripcion,
             clasificacionAudiencia: produccion.clasificacionAudiencia,
             calificacionPromedio: 0,
-            multimedia: produccion.multimedia,
-            productora: produccion.productora,
+            multimedia,
+            productora,
         };
         return this.http.post(API_URL, cuerpo).toPromise();
     }
@@ -47,14 +51,14 @@ export class ProduccionesService {
      * Actualiza la informacion de un producción
      * @param produccion editado
      */
-    actualizarProduccion(produccion: Produccion) {
+    actualizarProduccion(produccion: Produccion, multimedia: Multimedia, productora: Productora) {
         const cuerpo = {
             id: produccion.id,
             nombre: produccion.nombre,
             descripcion: produccion.descripcion,
             clasificacionAudiencia: produccion.clasificacionAudiencia,
-            multimedia: produccion.multimedia,
-            productora: produccion.productora,
+            multimedia,
+            productora,
         };
         return this.http.put(API_URL, cuerpo);
     }
