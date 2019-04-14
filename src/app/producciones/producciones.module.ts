bd30db756507f586e814bfede4c872f9e9c8c5e5
JuanFernandoCastaneda <jf.castaneda@uniandes.edu.co>
2019-04-14 00:49:41 -0500

    Corrección de errores 1

--- src/app/producciones/producciones.module.ts
+++ src/app/producciones/producciones.module.ts
@@ -2,7 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from '../app-routing/app-routing.module';
+import {MultimediaService} from '../multimedia/multimedia.service';
 import {ProduccionesService} from '../producciones/producciones.service';
+import {ProductorasService} from '../productoras/productoras.service';
 import { ProduccionesCrearComponent } from './producciones-crear/producciones-crear.component';
 import { ProduccionesListarComponent } from './producciones-listar/producciones-listar.component';
 
@@ -13,6 +15,6 @@ import { ProduccionesListarComponent } from './producciones-listar/producciones-
     AppRoutingModule,
     FormsModule,
   ],
-  providers: [ProduccionesService],
+  providers: [ProduccionesService, ProductorasService, MultimediaService],
 })
 export class ProduccionesModule { }
