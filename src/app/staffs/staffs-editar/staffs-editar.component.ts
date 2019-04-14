cc6ea53d292e3dd70d16fa208a5841594caa2670
JuanFernandoCastaneda <jf.castaneda@uniandes.edu.co>
2019-04-14 12:51:18 -0500

    Corrección 2

--- src/app/staffs/staffs-editar/staffs-editar.component.ts
+++ src/app/staffs/staffs-editar/staffs-editar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Staff} from '../staff';
 import {StaffsService} from '../staffs.service';
 
